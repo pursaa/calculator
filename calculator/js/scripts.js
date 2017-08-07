a5767897318358278a292a2ab3ff18315aa48a8a
Debbie Michel and Safiy Cham <safiy@>
2017-08-07 13:35:40 -0700

    Add temp conversion

--- calculator/js/scripts.js
+++ calculator/js/scripts.js
@@ -3,5 +3,40 @@ return number1 + number2;
 };
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
+//var result = add(number1, number2);
+//alert(result);
 
-alert(add(number1, number2));
+var sub = function(number1, number2) {
+  return number1 - number2;
+};
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+//var result = sub(number1, number2);
+//alert(result);
+
+var mult = function(number1, number2) {
+  return number1 * number2;
+};
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+//var result = mult(number1, number2);
+//alert(result);
+
+var div = function(number1, number2) {
+  return number1 / number2;
+};
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = add(number1, number2);
+
+alert(result);
+
+
+//T(°F) = T(°C) × 1.8 + 32
+
+var fahrenheit = function(number1) {
+  return (number1 * 1.8) + 32;
+};
+var number1 =parseInt(prompt("Enter Celsius"));
+
+alert(number1)
